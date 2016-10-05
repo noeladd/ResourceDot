6e67cb1b6eeed443f187774ab5c5736db7b10785
Ingrid Cronburg <ingrid257@gmail.com>
2016-10-05 17:36:30 -0400

    implementing search results view (#15)

--- browser/js/common/factories/resource_factory.js
+++ browser/js/common/factories/resource_factory.js
@@ -3,26 +3,26 @@ app.factory('ResourceFactory', function($http, DataFactory) {
 
 	ResourceFactory.getAll = function() {
 		return $http.get('/api/resources')
-		.then(DataFactory.getData)
+		.then(DataFactory.getData);
 	}
 	ResourceFactory.getAllByTag = function() {
 		var tagIds = [...arguments]
 		tagIds = tagIds.join(',');
 		//  '/api/resources?tagIds=1,2,3,'
 		return $http.get('/api/resources?tagIds=' + tagIds)
-		.then(DataFactory.getData)
+		.then(DataFactory.getData);
 	}
 	ResourceFactory.getAllByType = function(type) {
 		return $http.get('/api/resources?type=' + type)
-		.then(DataFactory.getData)
+		.then(DataFactory.getData);
 	}
 	ResourceFactory.getById = function(id) {
 		return $http.get('/api/resources/' + id)
-		.then(DataFactory.getData)
+		.then(DataFactory.getData);
 	}
 	ResourceFactory.post = function(data) {
 		return $http.post('/api/resources', data)
-		.then(DataFactory.getData)
+		.then(DataFactory.getData);
 	}
   ResourceFactory.like = function(id) {
     return $http.put('/api/resources/' + id + '/like');
@@ -30,7 +30,5 @@ app.factory('ResourceFactory', function($http, DataFactory) {
   ResourceFactory.dislike = function(id) {
     return $http.put('/api/resources/' + id + '/dislike');
   }
-
 	return ResourceFactory;
-
 });
