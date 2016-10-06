607f9dc4e357eaa9129a6e50670b8df5b045627a
Ingrid Cronburg <ingrid257@gmail.com>
2016-10-06 16:56:08 -0400

    starting user profile page

--- server/env/development.js
+++ server/env/development.js
@@ -12,9 +12,14 @@ module.exports = {
     callbackURL: 'INSERT_FACEBOOK_CALLBACK_HERE'
   },
   GOOGLE: {
-    clientID: 'INSERT_GOOGLE_CLIENTID_HERE',
-    clientSecret: 'INSERT_GOOGLE_CLIENT_SECRET_HERE',
-    callbackURL: 'INSERT_GOOGLE_CALLBACK_HERE'
+    clientID: '844072712782-ll5vp5bl6fksfh9gb816imh7o1662t2i.apps.googleusercontent.com',
+    clientSecret: 'fntVdrzWnN2rlbhvI-kMdqtT',
+    callbackURL: '/auth/google/callback'
+  },
+  GITHUB: {
+    clientID: '75d7ceb948cfa1e83418',
+    clientSecret: 'c348274e1aaecc2b2058e7a844f285c88240eaa1',
+    callbackURL: 'auth/github/callback'
   },
   LOGGING: true,
   NATIVE: true
