b61fa62e38c29068f0a00a2d0366b86cf713846e
Rachel Addleman <addlemanrachel@gmail.com>
2016-10-07 12:44:32 -0400

    finished guide factory

--- browser/js/common/factories/guide_factory.js
+++ browser/js/common/factories/guide_factory.js
@@ -26,4 +26,11 @@ app.factory('Guide Factory', function($http, DataFactory) {
    GuideFactory.removeResource = function(id, data){
        return $http.put('/api/guides/' + id + '/delete', data)
    }
+   GuideFactory.like = function(id) {
+       return $http.put('/api/guides/' + id + '/like');
+   }
+   GuideFactory.dislike = function(id){
+       return $http.put('/api/guides/' + id + '/dislike');
+   }
+   return GuideFactory
 })
