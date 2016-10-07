878661fd455ad96d9e995162a29aa50191de3298
Sungwon Ma <sma2@swarthmore.edu>
2016-10-07 17:59:31 -0400

    resource cards now display like-dislike counts and clickable tags

--- browser/js/common/directives/resource-card/resource-card.js
+++ browser/js/common/directives/resource-card/resource-card.js
@@ -1,11 +1,26 @@
-app.directive('resourceCard', function (ResourceFactory) {
+app.directive('resourceCard', function ($state, ResourceFactory) {
     return {
         restrict: 'E',
         templateUrl: 'js/common/directives/resource-card/resource-card.html',
         scope: true,
         link: function(scope) {
-          scope.like = ResourceFactory.like;
-          scope.dislike = ResourceFactory.dislike;
+			scope.like = function(id) {
+				ResourceFactory.like(id)
+				.then(function() {
+					scope.resource.likes += 1;
+				})
+			}
+
+			scope.dislike = function(id) {
+				ResourceFactory.dislike(id)
+				.then(function() {
+					scope.resource.dislikes += 1;
+				})
+			}
+
+			scope.searchByTag = function(id) {
+				$state.go('searchResults', {tagIds: id});
+			}
         }
-    };
+	}
 });
