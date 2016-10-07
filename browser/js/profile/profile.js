2552aa77c85ae36046eec063b7e595aa7f34b191
Ingrid Cronburg <ingrid257@gmail.com>
2016-10-07 12:16:13 -0400

    Profile (#18)
    * Develop (#16)
    
    * added a few models
    
    * finished first set of models
    
    * Beginning of routes changes to models
    
    * Routes (#1)
    
    * added a few models
    
    * finished first set of models
    
    * Beginning of routes changes to models
    
    * fixed resources routes
    
    * commit to checkout other branch
    
    * Changed type to query string.
     Added 404 error handling
    
    * cleaning up routes
    
    * Deleting conflict markers
    
    * Tests working
    
    * Fixed issue with association test
    
    * Test 1 working
    
    * resource route tests working
    
    * user tests model
    
    * Testing (#3)
    
    * commit to checkout other branch
    
    * Tests working
    
    * Fixed issue with association test
    
    * Test 1 working
    
    * resource route tests working
    
    * user tests model
    
    * User routes test almost done
    
    * Having issues with tags routes test?
    
    * All route and model tests written and passing!
    
    * Added a profile picture field.
    Also has an abstract placeholder as default.
    
    * changed the tag model to set title LowerCase.
    Removed members-only test
    
    * Changed resource associations to like & dislike, changed routes and tests to conform to change
    
    * Added factory files to browser/js/common/factories
    
    * made tag and user factory- beginnings
    
    * implemented resourceFactory methods
    
    * Resource Factory
    Added the get recommendations method
    
    * Factories (#8)
    
    * Added a profile picture field.
    Also has an abstract placeholder as default.
    
    * Changed resource associations to like & dislike, changed routes and tests to conform to change
    
    * Added factory files to browser/js/common/factories
    
    * made tag and user factory- beginnings
    
    * implemented resourceFactory methods
    
    * Resource Factory
    Added the get recommendations method
    
    * Changed formatting
    Added a getterMethod for netLikes to resource model.
    
    * DRY'd up factories
    
    * Changed resource factory to access by tags
    
    * Design (#12)
    
    * adding angular-material
    
    * chip tags are working
    
    * tag search route implemented
    
    * implementing tag search
    
    * Route changes (#13)
    
    * updated resources route and test to work with new model
    
    * removed method from tag route
    
    * Incorporated changes to resource routes
    Changed recommended algorithm to exclude resources you've already liked / disliked
    
    * removed typo
    
    * added guides and refactored
    
    * fixing data factory response
    
    * updating profile controller
    
    * eager loading on resource method
    
    * users can like/dislike resources
    
    * fixed response status
    
    * implementing search results view (#15)
    
    * starting user profile page
    
    * adding user sidebar
    
    * placed bower componenets in gitignore
    
    * took out bower_componenets

--- browser/js/profile/profile.js
+++ browser/js/profile/profile.js
@@ -1,34 +1,31 @@
 app.config(function ($stateProvider) {
   $stateProvider.state('profile', {
       url: '/profile',
-      controller: 'ProfileController',
+      controller: 'ProfileCtrl',
       templateUrl: 'js/profile/profile.html'
   });
-
 });
 
-app.controller('ProfileController', function ($scope, TagFactory, UserFactory, AuthService, $log, ResourceFactory, RecommendationFactory) {
-
+app.controller('ProfileCtrl', function ($scope, TagFactory, UserFactory, AuthService, $log, ResourceFactory, RecommendationFactory) {
   AuthService.getLoggedInUser()
   .then(function(user){
      return UserFactory.getById(user.id);
   })
   .then(function(fullUser){
-    $scope.user = fullUser;
-    $scope.userTags = fullUser.tags;
+    $scope.user = fullUser; // gets current user
+    $scope.userTags = fullUser.tags; // gets user's tags (topics user is interested in)
     var tags = fullUser.tags.map(function(tag){
       return tag.id;
     }).join(',');
-    return ResourceFactory.getAllByTag(tags);
+    return ResourceFactory.getAllByTag(tags); // gets all resources with user's tags
   })
   .then(function(resources){
-    $scope.resources = RecommendationFactory.get(resources, $scope.user).map(function(obj){
+    $scope.resources = RecommendationFactory.get(resources, $scope.user).map(function(obj){ // gets an array of recommended resources (many of them!)
       return obj.resource;
-    });
+    }).slice(0, 5);
   })
-  .catch($log.error)
+  .catch($log.error);
 
-  $scope.like = ResourceFactory.like;
-  $scope.dislike = ResourceFactory.dislike;
-
+  // $scope.like = ResourceFactory.like;
+  // $scope.dislike = ResourceFactory.dislike;
 });
