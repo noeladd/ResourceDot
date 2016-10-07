e96c2b45b551893100df05a4959f04864c59394c
Ingrid Cronburg <ingrid257@gmail.com>
2016-10-07 15:40:59 -0400

    Profile (#25)
    * starting user profile page
    
    * adding user sidebar
    
    * placed bower componenets in gitignore
    
    * took out bower_componenets
    
    * creating directive for tag chips

--- browser/js/profile/profile.js
+++ browser/js/profile/profile.js
@@ -7,22 +7,37 @@ app.config(function ($stateProvider) {
 });
 
 app.controller('ProfileCtrl', function ($scope, TagFactory, UserFactory, AuthService, $log, ResourceFactory, RecommendationFactory) {
+  $scope.selectedTags = [];
+
+  // profile page displays: recommended resources, guides created by the user, user's picture & account settings, & user's friends
+  function fetchResources() {
+    var tags = $scope.selectedTags.map(function(tag) {
+      return tag.id;
+    }).join(', ');
+
+    return ResourceFactory.getAllByTag(tags)
+    .then(function(resources) {
+      $scope.resources = RecommendationFactory.get(resources, $scope.user).map(function(obj){ // gets an array of recommended resources (many of them!)
+        return obj.resource;
+      }).slice(0, 5);
+    })
+    .catch($log.error);
+  }
+
+  var debounced = _.debounce(fetchResources, 1000);
+
   AuthService.getLoggedInUser()
   .then(function(user){
      return UserFactory.getById(user.id);
   })
   .then(function(fullUser){
-    $scope.user = fullUser;
-    $scope.userTags = fullUser.tags;
-    var tags = fullUser.tags.map(function(tag){
-      return tag.id;
-    }).join(',');
-    return ResourceFactory.getAllByTag(tags);
-  })
-  .then(function(resources){
-    $scope.resources = RecommendationFactory.get(resources, $scope.user).map(function(obj){
-      return obj.resource;
-    }).slice(0, 5);
+    $scope.user = fullUser; // gets current user
+    $scope.selectedTags = fullUser.tags; // gets user's tags (topics user is interested in)
+    return fetchResources();
   })
   .catch($log.error);
+
+  $scope.$watchCollection('selectedTags', function() {
+    debounced();
+  });
 });
