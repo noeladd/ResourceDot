3b4af4fd1a0e68a4d37e3371e8b8a81d116e8470
Mackenzie Turner <turner.mackenzie.m@gmail.com>
2016-10-13 11:40:56 -0400

    fixed add to guide bug

--- browser/js/profile/profile.js
+++ browser/js/profile/profile.js
@@ -22,21 +22,24 @@ app.controller('ProfileCtrl', function ($scope, $state, TagFactory, UserFactory,
 
   // profile page displays: recommended resources, guides created by the user, user's picture & account settings, & user's friends
   function fetchResources() {
-    var tags = $scope.selectedTags.map(function(tag) {
-      return tag.id;
-    }).join(', ');
+    if ($scope.selectedTags.length) {
+      var tags = $scope.selectedTags.map(function(tag) {
+        return tag.id;
+      }).join(', ');
 
-    var tagsArr = tags.split(', ');
+      var tagsArr = tags.split(', ');
 
-    UserFactory.setTags(user.id, tagsArr);
+      UserFactory.setTags(user.id, tagsArr);
 
-    return ResourceFactory.getAllByTag(tags)
-    .then(function(resources) {
-      $scope.resources = RecommendationFactory.get(resources, $scope.user).map(function(obj){
-        return obj.resource;
-      }).slice(0, 5);
-    })
-    .catch($log.error);
+      return ResourceFactory.getAllByTag(tags)
+      .then(function(resources) {
+        $scope.resources = RecommendationFactory.get(resources, $scope.user).map(function(obj){
+          return obj.resource;
+        }).slice(0, 5);
+      })
+      .catch($log.error);
+    }
+    else $scope.noTags = true;
   }
 
   $scope.viewLikedResources = function() {
@@ -53,10 +56,12 @@ app.controller('ProfileCtrl', function ($scope, $state, TagFactory, UserFactory,
     user = fullUser;
     $scope.user = fullUser; // gets current user
     $scope.selectedTags = fullUser.tags; // gets user's tags (topics user is interested in)
+    if (!fullUser.tags.length) $scope.noTags = true;
     $scope.friends = shuffleArray(user.friend).slice(0, 4);
     GuideFactory.getByAuthor(user.id)
     .then(function(guides) {
       $scope.guides = guides;
+      if (!$scope.guides.length) $scope.noGuides = true;
     })
     .catch($log.error);
     return fetchResources();
