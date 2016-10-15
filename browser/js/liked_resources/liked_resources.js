331634fc45dd2dadb7694e9ac9eeea2036c6e82b
Rachel Addleman <addlemanrachel@gmail.com>
2016-10-15 14:05:05 -0400

    Liked Resources Moved resolve to controller and added previously liked guides to the page.

--- browser/js/liked_resources/liked_resources.js
+++ browser/js/liked_resources/liked_resources.js
@@ -2,22 +2,21 @@ app.config(function($stateProvider) {
     $stateProvider.state('likedResources', {
       url: '/profile/:userId/liked',
       templateUrl: 'js/liked_resources/liked_resources.html',
-      controller: 'LikedResourcesCtrl',
-      resolve: {
-        user: function(UserFactory, $stateParams){
-          let id = $stateParams.userId;
-          return UserFactory.getById(id);
-        }
-      }
+      controller: 'LikedResourcesCtrl'
     });
 });
 
-app.controller('LikedResourcesCtrl', function($scope, user) {
-    $scope.likedResources = user.resourceLike;
+app.controller('LikedResourcesCtrl', function($scope, UserFactory, $stateParams) {
+  return UserFactory.getById($stateParams.userId)
+  .then(function(user){
+    console.log("USER! :", user)
     $scope.user = user;
-    $scope.guides = user.guides;
-    $scope.data = $scope.likedResources.slice(0, 5);
+    $scope.data = user.resourceLike.slice(0,5);
+    $scope.guides = user.guideLike;
+  })
+  .then(function(){
     $scope.getMoreData = function () {
-      $scope.data = $scope.likedResources.slice(0, $scope.data.length + 5)
+      $scope.data = $scope.user.resourceLike.slice(0, $scope.data.length + 5)
     }
+  })
 });
