3f41d4897d58ac883e49c1787b35512a74583cb3
Rachel Addleman <addlemanrachel@gmail.com>
2016-10-15 13:47:48 -0400

    moved resolve functions to controller friends.js

--- browser/js/friends/friends.js
+++ browser/js/friends/friends.js
@@ -2,18 +2,16 @@ app.config(function ($stateProvider) {
   $stateProvider.state('friends', {
       url: '/:userId/friends/all',
       templateUrl: 'js/friends/friends.html',
-      controller: 'friendsCtrl',
-      resolve: {
-        user: function(UserFactory, $stateParams) {
-          return UserFactory.getById($stateParams.userId);
-        }
-      }
+      controller: 'friendsCtrl'
   });
 });
 
-app.controller('friendsCtrl', function($scope, $state, user) {
-  $scope.friends = user.friend;
-
+app.controller('friendsCtrl', function($scope, $state, UserFactory, $stateParams) {
+  UserFactory.getById($stateParams.userId)
+  .then(function(user){
+    $scope.user = user;
+    $scope.friends = user.friend;
+  })
   $scope.findFriend = function(friendId) {
     $state.go('friend', {friendId: friendId});
   };
