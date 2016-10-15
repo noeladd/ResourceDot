9067f7f89d34f5eba9eac8f1023acfe5186315e6
Rachel Addleman <addlemanrachel@gmail.com>
2016-10-15 15:01:58 -0400

    search_people tweaks

--- browser/js/search_people/search_people.js
+++ browser/js/search_people/search_people.js
@@ -3,17 +3,14 @@ app.config(function ($stateProvider) {
       url: '/search_people',
       templateUrl: 'js/search_people/search_people.html',
       controller: 'searchPeopleCtrl',
-      resolve: {
-      users: function(UserFactory) {
-      	return UserFactory.getAll();
-      }
-    }
   });
 });
 
-app.controller('searchPeopleCtrl', function($scope, $state, users) {
-  //$scope.usersbyTag = usersByTag;
-  $scope.users = users
+app.controller('searchPeopleCtrl', function($scope, $state, UserFactory) {
+  UserFactory.getAll()
+  .then(function(users){
+    $scope.users = users;
+  })
   $scope.findFriend = function(userId){
     $state.go('friend', {friendId: userId})
   }
