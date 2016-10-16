3e26e4d537a029b0dbfe221b6bd7605f41aa3e0a
Sungwon Ma <sma2@swarthmore.edu>
2016-10-16 12:13:23 -0400

    dealt with add-to-guide, add-resoresource, search results, fab, guide-card, resource-card, navbar

--- browser/js/friend/friend.js
+++ browser/js/friend/friend.js
@@ -1,28 +1,29 @@
 app.config(function ($stateProvider) {
   $stateProvider.state('friend', {
-      url: '/friends/:friendId',
-      templateUrl: 'js/friend/friend.html',
-      controller: 'friendCtrl',
-      resolve: {
-        friend: function(UserFactory, $stateParams) {
-          return UserFactory.getById($stateParams.friendId);
-        },
-        guides: function(GuideFactory, $stateParams) {
-          return GuideFactory.getByAuthor($stateParams.friendId);
+    url: '/friends/:friendId',
+    templateUrl: 'js/friend/friend.html',
+    controller: 'friendCtrl',
+    resolve: {
+      friend: function(UserFactory, $stateParams) {
+        return UserFactory.getById($stateParams.friendId);
+      },
+      guides: function(GuideFactory, $stateParams) {
+        return GuideFactory.getByAuthor($stateParams.friendId);
 
-        },
-        user: function(AuthService, UserFactory){
-          return AuthService.getLoggedInUser()
-          .then(function(user){
-            if (!user){
-              return {id: 0, name: 'Guest', friend: []}
-            }
-            return UserFactory.getById(user.id);
-          })
-        }
+      },
+      user: function(AuthService, UserFactory){
+        return AuthService.getLoggedInUser()
+        .then(function(user){
+          if (!user){
+            return {id: 0, name: 'Guest', friend: []}
+          }
+          return UserFactory.getById(user.id);
+        })
       }
+    }
   });
 });
+
 app.controller('friendCtrl', function($scope, $state, UserFactory, friend, guides, user) {
   $scope.user = user;
   $scope.userFriends = $scope.user.friend;
